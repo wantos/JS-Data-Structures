ee6e39bde867cdcf3f216e0c456d1b6e3872215c
Wantono Emmanuel <51445540+wantos@users.noreply.github.com>
2019-06-19 09:11:26 +0300

    Update primenumber.js

--- primenumber.js
+++ primenumber.js
@@ -1,12 +1,13 @@
-function primenumber(num){
+function primenumber(num)//function name given primenumber with string num//
+{
  var array= [];
 
 for(var i = 1; i < 100; i++){
    if( num%i ===0){
    	return array;
-  }
-}
-}
+  };
+};
+};
 console.log(array);
 
 
